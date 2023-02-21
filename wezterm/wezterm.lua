f5c15df31206bbef52be7a41fa1acff32d4ad067
ryanss <ryanssdev@icloud.com>
2023-02-21 09:12:17 -0500

    Darken background in wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,4 +1,4 @@
-local wezterm = require 'wezterm'
+local wezterm = require "wezterm"
 
 local function change_opacity(window, delta)
   local overrides = window:get_config_overrides() or {}
@@ -7,22 +7,25 @@ local function change_opacity(window, delta)
   overrides.window_background_opacity = opacity
   window:set_config_overrides(overrides)
 end
-wezterm.on('opacity-decrease', function(window) change_opacity(window, -0.02) end)
-wezterm.on('opacity-increase', function(window) change_opacity(window,  0.02) end)
+wezterm.on("opacity-decrease", function(window) change_opacity(window, -0.02) end)
+wezterm.on("opacity-increase", function(window) change_opacity(window,  0.02) end)
 
 
 return {
   -- Font
-  font = wezterm.font 'CaskaydiaCove Nerd Font Mono',
+  font = wezterm.font "CaskaydiaCove Nerd Font Mono",
   font_size = 20,
   adjust_window_size_when_changing_font_size = false,
 
   -- Colors
-  color_scheme = 'nord',
+  color_scheme = "nord",
+  colors = {
+    background = "#232731",  -- nord0 default: #2E3440
+  },
 
   -- Windows
   window_background_opacity = 1.00,
-  window_decorations = 'RESIZE',
+  window_decorations = "RESIZE",
 
   -- Tabs
   hide_tab_bar_if_only_one_tab = true,
@@ -30,11 +33,11 @@ return {
   use_fancy_tab_bar = false,
 
   -- Misc
-  window_close_confirmation = 'NeverPrompt',
+  window_close_confirmation = "NeverPrompt",
 
   -- Key Binds
   keys = {
-    { key = '-', mods = 'CTRL', action = wezterm.action.EmitEvent('opacity-decrease') },
-    { key = '=', mods = 'CTRL', action = wezterm.action.EmitEvent('opacity-increase') },
+    { key = "-", mods = "CTRL", action = wezterm.action.EmitEvent("opacity-decrease") },
+    { key = "=", mods = "CTRL", action = wezterm.action.EmitEvent("opacity-increase") },
   },
 }
