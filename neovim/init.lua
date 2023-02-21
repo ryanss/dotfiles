7468ccf093692c1ca2e09c0887b8aad319f13ed7
ryanss <ryanssdev@icloud.com>
2023-02-21 10:18:52 -0500

    Add treesitter incremental selection keymaps

--- neovim/init.lua
+++ neovim/init.lua
@@ -138,6 +138,14 @@ require("lazy").setup({
       require("nvim-treesitter.configs").setup({
         auto_install = true,
         highlight = { enable = true },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "+",
+            node_incremental = "+",
+            node_decremental = "_",
+          },
+        },
       })
     end,
   },
