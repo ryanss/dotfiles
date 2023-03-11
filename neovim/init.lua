3101aad91c3e458b612337f01d48b783f2177971
ryanss <ryanssdev@icloud.com>
2023-03-10 19:39:13 -0500

    Add nvim-ts-context-commentstring plugin

--- neovim/init.lua
+++ neovim/init.lua
@@ -441,7 +441,23 @@ require("lazy").setup({
 
   { -- Smart and powerful comment plugin for neovim
     "numToStr/Comment.nvim",
-    config = true,
+    config = function()
+      require("Comment").setup({
+        pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+      })
+    end,
+  },
+
+  { -- Nvim TS plugin for setting the commentstring based on the cursor location
+    "JoosepAlviste/nvim-ts-context-commentstring",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        context_commentstring = {
+          enable = true,
+          enable_autocmd = false,
+        },
+      })
+    end,
   },
 
   { -- The fastest Neovim colorizer
