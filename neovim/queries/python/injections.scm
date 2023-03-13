53f5a09f0ab590b8103ff2ddcf1b133e1dd00656
ryanss <ryanssdev@icloud.com>
2023-03-13 10:52:09 -0400

    Inject html treesitter highlighting in python strings with html elements

--- neovim/queries/python/injections.scm
+++ neovim/queries/python/injections.scm
@@ -1,5 +1,8 @@
 ; extends
 
+((string) @html
+ (#contains? @html "<a" "<b" "<h" "<i" "<p"))
+
 ((string) @sql
  (#contains? @sql "SELECT" "INSERT" "UPDATE" "DELETE" "CREATE" "REPLACE" "ALTER"
                   "select" "insert" "update" "delete" "create" "replace" "alter"))
